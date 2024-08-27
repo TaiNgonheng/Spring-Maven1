33cdeeabf01cfe61affa43c60c5164eb32af2c1b
watermelon <heng68807@gmail.com>
2024-08-27 18:37:00 +0700

    Initial commit

---
+++ src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/LearnSpringFrameworkApplication.java
@@ -0,0 +1,15 @@
+package com.Tai_Ngonheng.Learn_Spring_Framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		System.out.println(" Hello world");
+
+	}
+
+}
---
+++ src/test/java/com/Tai_Ngonheng/Learn_Spring_Framework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tai_Ngonheng.Learn_Spring_Framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
