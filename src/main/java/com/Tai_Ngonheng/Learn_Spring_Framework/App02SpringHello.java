e0aaa5b13b4933dd3ef69858064d7a8974b8a8b6
watermelon <heng68807@gmail.com>
2024-08-29 17:02:22 +0700

    Configure Spring

--- src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
+++ src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
@@ -5,8 +5,5 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App02SpringHello {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-
-
-
     }
 }
