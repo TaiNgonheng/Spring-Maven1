030556f8b8624c16865ce5e161726c6ede839b49
watermelon <heng68807@gmail.com>
2024-08-29 17:33:57 +0700

    maven

--- src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
+++ src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
@@ -6,7 +6,5 @@ public class App02SpringHello {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         System.out.println(context.getBean("name"));
-
-
     }
 }
