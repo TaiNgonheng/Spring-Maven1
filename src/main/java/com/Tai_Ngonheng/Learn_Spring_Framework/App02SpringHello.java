f2d102f4b55a92dae49614f86c0fa2fc7130bb0a
watermelon <heng68807@gmail.com>
2024-08-29 16:54:14 +0700

    MarioGame and ContraGame Create

--- src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
+++ src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
@@ -5,7 +5,5 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App02SpringHello {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-        System.out.println(context.getBean("BMW"));;
-        System.out.println(context.getBean("heng"));
     }
 }
