0ab0226fb245e5bb8c306caf3168693fe5571d24
watermelon <heng68807@gmail.com>
2024-08-29 22:30:31 +0700

    Bean and Method

--- src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
+++ src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
@@ -6,8 +6,11 @@ public class App02SpringHello {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         System.out.println(context.getBean("name"));
-
-
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("Address"));
+        System.out.println(context.getBean("information2"));
+        System.out.println(context.getBean("person2method"));
 
     }
 }
