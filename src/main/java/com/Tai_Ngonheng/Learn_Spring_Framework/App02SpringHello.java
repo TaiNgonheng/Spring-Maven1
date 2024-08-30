71ac6f7f245a7c8a04d0eb621a10f4e17125d02f
watermelon <heng68807@gmail.com>
2024-08-30 17:51:51 +0700

    parameter use for function

--- src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
+++ src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/App02SpringHello.java
@@ -11,6 +11,7 @@ public class App02SpringHello {
         System.out.println(context.getBean("Address"));
         System.out.println(context.getBean("information2"));
         System.out.println(context.getBean("person2method"));
-
+        System.out.println(context.getBean(test.class));
+        System.out.println(context.getBean("paramenter"));
     }
 }
