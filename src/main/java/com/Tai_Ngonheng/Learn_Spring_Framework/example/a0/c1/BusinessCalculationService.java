4d38fe8ec97eab839acada304ba7f2e8f991e7e6
TaiNgonheng <heng68807@gmail.com>
2024-09-04 14:29:43 +0700

    solution on a0-c1

--- src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/example/a0/c1/BusinessCalculationService.java
+++ src/main/java/com/Tai_Ngonheng/Learn_Spring_Framework/example/a0/c1/BusinessCalculationService.java
@@ -14,8 +14,6 @@ public class BusinessCalculationService {
         this.dataService= dataService;
     }
 
-
-
     public int findMax(){
 
         return Arrays.stream(dataService.retrieveData()).max().orElse(0);
